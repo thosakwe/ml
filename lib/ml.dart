4973edb5885184e7765f17f9eb78f16a11d4f4c5
Tobe O <thosakwe@gmail.com>
2018-06-29 20:02:15 -0400

    C Compiler skeleton

--- lib/ml.dart
+++ lib/ml.dart
@@ -1,2 +1,3 @@
+export 'src/c_compiler/c_compiler.dart';
 export 'src/framework/framework.dart';
 export 'src/models/models.dart';
--- lib/src/framework/compiler.dart
+++ lib/src/framework/compiler.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:ml/ml.dart';
 
+/// On-the-fly compiles a [program] into a native executable, so that it can be run.
 abstract class Compiler {
+  Future<RunnableProgram> compile(ProgramNode node);
 }
